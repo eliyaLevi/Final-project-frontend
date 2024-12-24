6443dabd091cefdc549cb8155ad5a101a77f3c30
eliyaLevi <el0547135208@gmail.com>
2024-12-24 16:04:25 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
-import { Box, Container } from "@mui/material";
 import { Layout } from "./componnets/layout/Layout";
 import { AppRouter } from "./router/AppRouter";
-import { DeadliestAttack } from "./pages/deadliest-attack/DeadliestAttack";
-import { Casualty_regions } from "./pages/casualty_regions/Casualty_regions";
-import { Incident_trends } from "./pages/incident-trends/Incident_trends";
-import { HomePage } from "./pages/home-page/HomePage";
 
 function App() {
   return (
